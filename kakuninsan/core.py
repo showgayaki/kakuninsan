11fce24ca1b0ada9ff7f83fe223807336b127f00
showgayaki <knk.lee.kj@gmail.com>
2021-04-11 10:39:07 +0900

    ログ出力形式変更

--- kakuninsan/core.py
+++ kakuninsan/core.py
@@ -61,7 +61,7 @@ def main():
     # computer_name取得
     computer_name = socket.gethostname()
     level = 'info'
-    log.logging(level, 'Started on {}'.format(computer_name))
+    log.logging(level, 'Kakuninsan Started on {}'.format(computer_name))
 
     # DBインスタンス
     db = TableIp(cfg['db_info'], cfg['table_detail']['table_name'])
@@ -69,89 +69,98 @@ def main():
     # スピードテスト
     retry_count = 3
     st = SpeedTest()
+    log.logging(level, '---Server list---')
     server_list = st.sponsor()
-    log.logging(level, 'Server list: {}'.format(server_list))
+    server_ids = []
+    for i, item in enumerate(server_list.items()):
+        server_ids.append(item[0])
+        log.logging(level, 'Server{}: {}'.format(i + 1, item))
+    log.logging(level, '-----------------')
+
     for i in range(1, retry_count + 1):
         level = 'info'
         log.logging(level, 'Start SpeedTest, count: {}'.format(i))
-        st_result = st.speed_test_result(server_list)
 
-        if 'Error' in st_result.keys():
-            level = 'error'
-            log.logging(level, 'SpeedTest Failed: {}'.format(st_result['Error']))
-            time.sleep(3)
-            continue
-        else:
-            current_ip = st_result['global_ip_address']
-            log.logging(level, 'Current IP Address: {}'.format(current_ip))
-            log.logging(level, 'Sponsor: {}'.format(st_result['sponsor']))
+        for server_id in server_ids:
+            st_result = st.speed_test_result(server_id)
 
-            download = graph.bytes_to_megabytes(st_result['download'])
-            upload = graph.bytes_to_megabytes(st_result['upload'])
-            log.logging(level, 'Download Speed: {} Mbps'.format(download))
-            log.logging(level, 'Upload Speed: {} Mbps'.format(upload))
+            if 'Error' in st_result.keys():
+                level = 'error'
+                log.logging(level, 'SpeedTest Failed: {}'.format(st_result['Error']))
+                time.sleep(3)
+                continue
+            else:
+                current_ip = st_result['global_ip_address']
+                log.logging(level, 'Current IP Address: {}'.format(current_ip))
+                log.logging(level, 'Sponsor: {}'.format(st_result['sponsor']))
 
-            # Insert
-            log.logging(level, 'Start DB insert')
-            insert_dict = insert_info(now, computer_name, st_result)
-            insert_result = db.insert_record(cfg['db_info'], cfg['table_detail'], insert_dict)
-            level = 'error' if 'Error' in insert_result else 'info'
-            log.logging(level, 'DB insert {}'.format(insert_result))
+                download = graph.bytes_to_megabytes(st_result['download'])
+                upload = graph.bytes_to_megabytes(st_result['upload'])
+                log.logging(level, 'Download Speed: {} Mbps'.format(download))
+                log.logging(level, 'Upload Speed: {} Mbps'.format(upload))
 
-            interval_hour = int(cfg['interval_hour']) if cfg['interval_hour'] else 24
-            log.logging(level, 'Start fetch last ip')
-            records = db.fetch_last_ip(cfg['table_detail']['clm_created_at'], interval_hour)
-            level = 'error' if 'Error' in records else 'info'
-            # 前回のIPは、今回インサートしたものの一つ前(listの2番目)のレコード
-            last_ip = records[1][1]
-            log.logging(level, 'Last IP Address: {}'.format(last_ip))
+                # Insert
+                log.logging(level, 'Start DB insert')
+                insert_dict = insert_info(now, computer_name, st_result)
+                insert_result = db.insert_record(cfg['db_info'], cfg['table_detail'], insert_dict)
+                level = 'error' if 'Error' in insert_result else 'info'
+                log.logging(level, 'DB insert {}'.format(insert_result))
 
-            is_send_time = now.strftime('%H') == cfg['mail_send_time']
-            is_post_time = now.strftime('%H') == cfg['line']['post_time']
-            # 指定時間になったらメール送信 or LINEで通知。指定時間以外は、webサーバー動いている環境ならindex.htmlに書き出し
-            if is_send_time or is_post_time or cfg['web_server']['is_running']:
-                level = 'info'
-                # グラフ画像
-                grph = Graph(records)
-                image_file_path = grph.draw_graph()
-                # コンテンツ作成
-                html = Html(root_dir)
+                interval_hour = int(cfg['interval_hour']) if cfg['interval_hour'] else 24
+                log.logging(level, 'Start fetch last ip')
+                records = db.fetch_last_ip(cfg['table_detail']['clm_created_at'], interval_hour)
+                level = 'error' if 'Error' in records else 'info'
+                # 前回のIPは、今回インサートしたものの一つ前(listの2番目)のレコード
+                last_ip = records[1][1]
+                log.logging(level, 'Last IP Address: {}'.format(last_ip))
 
-                if is_send_time:
-                    log.logging(level, 'It is time to send an email')
-                    is_updated, records = check_ip(records)
-                    mail_contents = html.build_html(False, records, image_file_path)
-                    subject = 'IP Address is UPDATED' if is_updated else 'IP Address is NOT updated'
-                    body_dict = {'subject': subject, 'body': mail_contents}
-                    # メール送信
-                    log.logging(level, 'Start to send email')
-                    mailer = Mail(cfg['mail_info'])
-                    msg = mailer.create_message(body_dict)
-                    result = mailer.send_mail(msg)
-                    level = 'error' if 'Error' in result else 'info'
-                    log.logging(level, 'Send Mail Result {}'.format(result))
-                else:
-                    log.logging(level, 'It is not time to send an email')
+                is_send_time = now.strftime('%H') == cfg['mail_send_time']
+                is_post_time = now.strftime('%H') == cfg['line']['post_time']
+                # 指定時間になったらメール送信 or LINEで通知。指定時間以外は、webサーバー動いている環境ならindex.htmlに書き出し
+                if is_send_time or is_post_time or cfg['web_server']['is_running']:
+                    level = 'info'
+                    # グラフ画像
+                    grph = Graph(records)
+                    image_file_path = grph.draw_graph()
+                    # コンテンツ作成
+                    html = Html(root_dir)
 
-                if cfg['web_server']['is_running']:
-                    is_updated, records = check_ip(records)
-                    web_contents = html.build_html(True, records, image_file_path)
-                    # htmlフォルダなかったら作って、index.htmlに書き出し
-                    index_dir = Path(cfg['web_server']['document_root'])
-                    if not index_dir.is_dir():
-                        index_dir.mkdir()
-                    index_path = index_dir.joinpath('index.html')
-                    with open(index_path, 'w', encoding='utf-8') as f:
-                        f.write(web_contents)
+                    if is_send_time:
+                        log.logging(level, 'It is time to send an email')
+                        is_updated, records = check_ip(records)
+                        mail_contents = html.build_html(False, records, image_file_path)
+                        subject = 'IP Address is UPDATED' if is_updated else 'IP Address is NOT updated'
+                        body_dict = {'subject': subject, 'body': mail_contents}
+                        # メール送信
+                        log.logging(level, 'Start to send email')
+                        mailer = Mail(cfg['mail_info'])
+                        msg = mailer.create_message(body_dict)
+                        result = mailer.send_mail(msg)
+                        level = 'error' if 'Error' in result else 'info'
+                        log.logging(level, 'Send Mail Result {}'.format(result))
+                    else:
+                        log.logging(level, 'It is not time to send an email')
 
-                if is_post_time:
-                    post_result = post_line(cfg['line']['api_url'], cfg['line']['access_token'], image_file_path)
-                    level = 'error' if 'Error' in post_result else 'info'
-                    log.logging(level, 'LINE result: {}'.format(post_result))
+                    if cfg['web_server']['is_running']:
+                        is_updated, records = check_ip(records)
+                        web_contents = html.build_html(True, records, image_file_path)
+                        # htmlフォルダなかったら作って、index.htmlに書き出し
+                        index_dir = Path(cfg['web_server']['document_root'])
+                        if not index_dir.is_dir():
+                            index_dir.mkdir()
+                        index_path = index_dir.joinpath('index.html')
+                        with open(index_path, 'w', encoding='utf-8') as f:
+                            f.write(web_contents)
+
+                    if is_post_time:
+                        post_result = post_line(cfg['line']['api_url'], cfg['line']['access_token'], image_file_path)
+                        level = 'error' if 'Error' in post_result else 'info'
+                        log.logging(level, 'LINE result: {}'.format(post_result))
+                break
         break
 
     level = 'info'
-    log.logging(level, 'Stopped.')
+    log.logging(level, 'Kakuninsan Stopped.')
 
 
 if __name__ == '__main__':
--- kakuninsan/speed_test.py
+++ kakuninsan/speed_test.py
@@ -12,28 +12,35 @@ class SpeedTest:
         p1 = subprocess.Popen(['speedtest', '--list'], encoding='utf-8', stdout=subprocess.PIPE)
         p2 = subprocess.Popen(['grep', 'Japan'], encoding='utf-8', stdin=p1.stdout, stdout=subprocess.PIPE)
         p1.stdout.close()
-        # Server IDを抜き出してリスト化
-        server_list = [server.split(')')[0].strip() for server in p2.communicate()[0].split('\n')][:self.SERVER_COUNT]
+        # Serverを抜き出してリスト化
+        server_list = {}
+        for server in p2.communicate()[0].split('\n')[:self.SERVER_COUNT]:
+            # 14623) IPA CyberLab (Bunkyo, Japan)[2.47 km]
+            # 上記の形で出力されるので、整形してdictにする
+            server_id = server.split(') ')[0]
+            server_list[server_id] = {}
+            server_list[server_id]['sponsor'] = server.split(') ')[1].split(' (')[0]
+            server_list[server_id]['server_area'] = server.split(') ')[1].split(' (')[1]
+            server_list[server_id]['distance'] = server.split(') ')[2].replace('[', '').replace(']', '')
+
         return server_list
 
-    def speed_test_result(self, server_list):
+    def speed_test_result(self, server_id):
         self.options.append('--server')
+        self.options.append(server_id)
         result_dict = {}
-        for server in server_list:
-            self.options.append(server)
-            try:
-                process = subprocess.run(self.options, encoding='utf-8', stdout=subprocess.PIPE)
-                result = [line for line in process.stdout.split('\n')]
-                res_json = json.loads(result[0])
-                result_dict = {
-                    'download': res_json['download'],
-                    'upload': res_json['upload'],
-                    'global_ip_address': res_json['client']['ip'],
-                    'sponsor': res_json['server']['sponsor'],
-                    'image_url': res_json['share']
-                }
-                return result_dict
-            except Exception as e:
-                result_dict['Error'] = {}
-                result_dict['Error'][server] = e
+        try:
+            process = subprocess.run(self.options, encoding='utf-8', stdout=subprocess.PIPE)
+            result = [line for line in process.stdout.split('\n')]
+            res_json = json.loads(result[0])
+            result_dict = {
+                'download': res_json['download'],
+                'upload': res_json['upload'],
+                'global_ip_address': res_json['client']['ip'],
+                'sponsor': res_json['server']['sponsor'],
+                'image_url': res_json['share']
+            }
+            return result_dict
+        except Exception as e:
+            result_dict['Error'] = e
         return result_dict
